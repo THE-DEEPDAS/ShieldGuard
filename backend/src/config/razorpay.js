5ac0537900b2526889c2192d5730c3b4326c3e0f
THE-DEEPDAS <deepdblm@gmail.com>
2025-03-14 23:44:11 +0530

    fallback to env

--- backend/src/config/razorpay.js
+++ backend/src/config/razorpay.js
@@ -1,4 +1,12 @@
 import Razorpay from "razorpay";
+import dotenv from "dotenv";
+dotenv.config();
+
+if (!process.env.key_id || !process.env.key_secret) {
+  console.error(
+    "WARNING: Razorpay API keys are missing in environment variables"
+  );
+}
 
 const razorpayInstance = new Razorpay({
   key_id: process.env.key_id,
