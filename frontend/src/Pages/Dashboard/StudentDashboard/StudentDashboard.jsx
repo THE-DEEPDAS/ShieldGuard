7844e0c864db817b1e3bbaa87deff218cefcfb1c
THE-DEEPDAS <deepdblm@gmail.com>
2025-04-09 20:42:24 +0530

    respectable dashboards

--- frontend/src/Pages/Dashboard/StudentDashboard/StudentDashboard.jsx
+++ frontend/src/Pages/Dashboard/StudentDashboard/StudentDashboard.jsx
@@ -49,24 +49,24 @@ function StudentDashboard() {
   return (
     <>
       {/* navbar */}
-      <nav className="bg-[#04253A] px-10 py-3 flex justify-between items-center">
+      <nav className="bg-[#2d0808] px-10 py-3 flex justify-between items-center">
         <NavLink to="/">
           <div className="flex items-center gap-3">
             <img src={logo} className="w-14" alt="" />
-            <h1 className="text-2xl text-[#4E84C1] font-bold">ShieldGuard</h1>
+            <h1 className="text-2xl text-[#caae71] font-bold">ShieldGuard</h1>
           </div>
         </NavLink>
-        <div className="bg-[#0D199D] text-white py-2 px-5 rounded-full">
+        <div className="bg-[#caae71] text-[#2d0808] py-2 px-5 rounded-full">
           <p onClick={Handlelogout}>logout</p>
         </div>
       </nav>
 
-      <div className="bg-[#008280] flex justify-between items-center">
-        <div className=" text-white font-semibold text-5xl ml-72">
-          <h1 className="mb-5 text-[#071645]">
-            Welcome to <span className="text-white">ShieldGuard</span>
+      <div className="bg-[#caae71] flex justify-between items-center w-full">
+        <div className="text-[#2d0808] font-semibold text-5xl ml-20">
+          <h1 className="mb-5">
+            Welcome to <span className="text-[#2d0808]">ShieldGuard</span>
           </h1>
-          <h3 className="ml-16 text-[#071645]">
+          <h3 className="ml-16 text-[#2d0808]">
             {data.Firstname} {data.Lastname}
           </h3>
         </div>
@@ -76,8 +76,8 @@ function StudentDashboard() {
       </div>
 
       {/* sidebar */}
-      <div className="bg-[#071645] w-52 min-h-[120vh] max-h-[130vh] absolute top-20">
-        <div className="flex flex-col gap-5 text-xl items-center text-white mt-8 mb-10">
+      <div className="bg-[#2d0808] w-52 min-h-screen fixed top-20 left-0">
+        <div className="flex flex-col gap-5 text-xl items-center text-[#caae71] mt-8 mb-10">
           <img
             src="https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png"
             alt="profile_img"
@@ -93,8 +93,8 @@ function StudentDashboard() {
             to={`/Student/Dashboard/${ID}/Search`}
             className={({ isActive }) =>
               isActive
-                ? "bg-white p-3 px-[4.61rem] text-center font-semibold text-[#4E84C1]"
-                : "p-3 text-center font-semibold text-[#4E84C1]"
+                ? "bg-[#caae71] p-3 px-[4.61rem] text-center font-semibold text-[#2d0808]"
+                : "p-3 text-center font-semibold text-[#caae71]"
             }
           >
             Teacher
@@ -104,8 +104,8 @@ function StudentDashboard() {
             to={`/Student/Dashboard/${ID}/Classes`}
             className={({ isActive }) =>
               isActive
-                ? "bg-white p-3 px-[4.61rem] text-center font-semibold text-[#4E84C1]"
-                : "p-3 text-center font-semibold text-[#4E84C1]"
+                ? "bg-[#caae71] p-3 px-[4.61rem] text-center font-semibold text-[#2d0808]"
+                : "p-3 text-center font-semibold text-[#caae71]"
             }
           >
             Classes
@@ -115,8 +115,8 @@ function StudentDashboard() {
             to={`/Student/Dashboard/${ID}/Courses`}
             className={({ isActive }) =>
               isActive
-                ? "bg-white p-3 px-[4.61rem] text-center font-semibold text-[#4E84C1]"
-                : "p-3 text-center font-semibold text-[#4E84C1]"
+                ? "bg-[#caae71] p-3 px-[4.61rem] text-center font-semibold text-[#2d0808]"
+                : "p-3 text-center font-semibold text-[#caae71]"
             }
           >
             Courses
--- frontend/src/Pages/Dashboard/TeacherDashboard/DashboardTeacher.jsx
+++ frontend/src/Pages/Dashboard/TeacherDashboard/DashboardTeacher.jsx
@@ -11,7 +11,7 @@ function DashboardTeacher() {
   const [popup, setPopup] = useState(false);
   const [notification, setNotification] = useState(false);
   const [amount, setAmount] = useState(0);
-  const [subjectForm, setsubjectForm] = useState('Math');
+  const [subjectForm, setsubjectForm] = useState("Math");
   const [Tdec, setTeacherDetails] = useState(null);
   const [starCount, setStar] = useState(5);
 
@@ -25,7 +25,15 @@ function DashboardTeacher() {
     biology: 500,
   };
 
-  const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   useEffect(() => {
     const getData = async () => {
@@ -51,23 +59,23 @@ function DashboardTeacher() {
     getData();
   }, []);
 
-  useEffect(()=>{
-    const getData = async()=>{
-      const Data = await fetch('/api/teacher/teacherdocuments',{
-        method: 'POST',
+  useEffect(() => {
+    const getData = async () => {
+      const Data = await fetch("/api/teacher/teacherdocuments", {
+        method: "POST",
         credentials: "include",
         headers: {
-        "Content-Type": "application/json",
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify({teacherID : data.Teacherdetails}),
-      })
+        body: JSON.stringify({ teacherID: data.Teacherdetails }),
+      });
       const res = await Data.json();
       // console.log(res.data);
       setTeacherDetails(res.data);
-    }
+    };
 
     getData();
-  },[courses])
+  }, [courses]);
 
   useEffect(() => {
     const getAmount = async () => {
@@ -120,101 +128,121 @@ function DashboardTeacher() {
 
   return (
     <>
-      <div className="m-5 ml-60 text-white flex flex-col gap-7">
+      <div className="m-5 ml-20 text-[#caae71] flex flex-col gap-7 w-full">
         <div className="text-[1.1rem] w-[30rem] flex gap-60 items-center">
-          {/* <p>Amount: <span className=" text-green-500">Rs. {amount}</span></p> */}
-          <div className="bg-[#1671D8] p-3 rounded-md cursor-pointer">
+          <div className="bg-[#2d0808] p-3 rounded-md cursor-pointer text-[#caae71]">
             Details
           </div>
-          <div
-            // onClick={() => setPopup(true)}
-            className="bg-[#1671D8] p-3 rounded-md cursor-pointer"
-          >
+          <div className="bg-[#2d0808] p-3 rounded-md cursor-pointer text-[#caae71]">
             Remuneration
           </div>
-          {/* <div className="flex items-center gap-2 ml-28 bg-[#1671D8] p-3 rounded-md cursor-pointer" onClick={()=>setNotification(prev => !prev)}>
-            <span>Notifications</span>
-            <TbMessage2Star />
-          </div> */}
         </div>
         <hr />
         <div className="flex gap-32">
           <div className="flex flex-col gap-5">
-            <p>Name: <span className="text-black">{data.Firstname} {data.Lastname}</span></p>
-            {/* <p>Name: {data.Firstname} {data.Lastname} {'⭐'.repeat(starCount)}</p> */}
-            <p>Email: <span className="text-black">{data.Email}</span></p>
-            <p>Phone: <span className="text-black">{Tdec?.Phone}</span></p>
-            <p>Address: <span className="text-black">{Tdec?.Address}</span></p>
-            <p>Experience: <span className="text-black">{Tdec?.Experience} years</span></p>
+            <p>
+              Name:{" "}
+              <span className="text-[#2d0808]">
+                {data.Firstname} {data.Lastname}
+              </span>
+            </p>
+            <p>
+              Email: <span className="text-[#2d0808]">{data.Email}</span>
+            </p>
+            <p>
+              Phone: <span className="text-[#2d0808]">{Tdec?.Phone}</span>
+            </p>
+            <p>
+              Address: <span className="text-[#2d0808]">{Tdec?.Address}</span>
+            </p>
+            <p>
+              Experience:{" "}
+              <span className="text-[#2d0808]">{Tdec?.Experience} years</span>
+            </p>
           </div>
           <div>
             <div className="flex gap-3 flex-col">
-              <p className="bg-[#1671D8] py-1 px-2 w-fit">Courses</p>
+              <p className="bg-[#2d0808] py-1 px-2 w-fit text-[#caae71]">
+                Courses
+              </p>
               {courses &&
-                courses.filter((course) => course.isapproved)
-                .map((course) => (
-                  <p
-                    key={course._id}
-                    // className=" bg-[#1671D8] py-1 px-2 rounded-xl w-fit"
-                    className="py-1 px-2 rounded-xl w-fit"
-                  >
-                    {course.coursename} :{" "}
-                    <span className="text-black">
-                      {" [ "}{course.schedule.map(days => `${daysOfWeek[days.day]} ${Math.floor(days.starttime/60)}:${(days.starttime%60 === 0 ? "00":days.starttime%60)} - ${Math.floor(days.endtime/60)}:${(days.endtime%60 === 0 ? "00" : days.endtime%60)}`).join(', ')}{" ] "}
-                    </span>
-                    <span className="text-black font-bold">
-                      {" => "}
-                      Rs. {price[course.coursename]} per student / per month
-                    </span>
-                  </p>
-                ))}
+                courses
+                  .filter((course) => course.isapproved)
+                  .map((course) => (
+                    <p
+                      key={course._id}
+                      className="py-1 px-2 rounded-xl w-fit text-[#caae71]"
+                    >
+                      {course.coursename} :{" "}
+                      <span className="text-[#2d0808]">
+                        {" [ "}
+                        {course.schedule
+                          .map(
+                            (days) =>
+                              `${daysOfWeek[days.day]} ${Math.floor(
+                                days.starttime / 60
+                              )}:${
+                                days.starttime % 60 === 0
+                                  ? "00"
+                                  : days.starttime % 60
+                              } - ${Math.floor(days.endtime / 60)}:${
+                                days.endtime % 60 === 0
+                                  ? "00"
+                                  : days.endtime % 60
+                              }`
+                          )
+                          .join(", ")}
+                        {" ] "}
+                      </span>
+                      <span className="text-[#2d0808] font-bold">
+                        {" => "}
+                        Rs. {price[course.coursename]} per student / per month
+                      </span>
+                    </p>
+                  ))}
             </div>
-          </div>
-          <div className="ml-28">
-            {/* {notification && (
-              show all notifications
-              example
-              <div>
-                <p>course : Math</p>
-                <p>Timing : sun,Mon,tue</p>
-                <p>status : pending</p>
-                <p>message : sbcxbbdjbd</p>
-              </div>
-            )} */}
           </div>
         </div>
 
         {popup && <Withdrawal onClose={() => setPopup(false)} TA={amount} />}
-        
+
         {formPopup && (
-          <div className='fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center'>
-            <div className='bg-[#5be0de] text-black w-[70vw] px-14 py-10 rounded-sm'>
-              {/* <div className=' absolute w-9 h-9 bg-white rounded-xl cursor-pointer flex items-center justify-center m-2' onClick={onClose}>✖️</div> */}
+          <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center">
+            <div className="bg-[#5be0de] text-black w-[70vw] px-14 py-10 rounded-sm">
+              <p className="text-3xl">Teacher Feedback Form</p>
+              <p className=" border-b-2 py-2">
+                We highly appreciate your involvement. Please help us improve by
+                filling out this teacher feedback form. Thank you!
+              </p>
 
-              <p className='text-3xl'>Teacher Feedback Form</p>
-              <p className=' border-b-2 py-2'>We highly appreciate your involvement. Please help us improve by filling out this teacher feedback form. Thank you!</p>
-
-              <div className='flex flex-col gap-3 my-5 pb-5 border-b-2'>
+              <div className="flex flex-col gap-3 my-5 pb-5 border-b-2">
                 <label>Full Name</label>
-                <input type="text" className='p-2'  placeholder='Teacher / Instructor Name'/>
+                <input
+                  type="text"
+                  className="p-2"
+                  placeholder="Teacher / Instructor Name"
+                />
                 <label>Course Name</label>
 
-                <input type="text" className='p-2'  placeholder='Course Name'/>
-                {/* <input type="text" value={subjectForm} readOnly className='p-2'  placeholder='Course Name'/> */}
-
+                <input type="text" className="p-2" placeholder="Course Name" />
                 <label>Number of Years Teaching ?</label>
-                <input type="text" className='p-2'  placeholder='in years'/>
+                <input type="text" className="p-2" placeholder="in years" />
               </div>
 
-              <div className='py-3 flex flex-col justify-center items-center'>
-                <p className='pb-3 text-center'>Do you have suggestions on what we can do to provide you with a better service?</p>
-                <textarea className=" rounded-md w-[80%] h-32 p-2" placeholder="Type here ..."></textarea>
+              <div className="py-3 flex flex-col justify-center items-center">
+                <p className="pb-3 text-center">
+                  Do you have suggestions on what we can do to provide you with
+                  a better service?
+                </p>
+                <textarea
+                  className=" rounded-md w-[80%] h-32 p-2"
+                  placeholder="Type here ..."
+                ></textarea>
               </div>
 
-              <div className='flex justify-center mt-3'>
-                <button className='w-[10rem]'>Submit Form</button>
+              <div className="flex justify-center mt-3">
+                <button className="w-[10rem]">Submit Form</button>
               </div>
-              
             </div>
           </div>
         )}
