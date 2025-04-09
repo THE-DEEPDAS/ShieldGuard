c6ebd4f147eca001603d4a229b8a92f2958854ea
pro-jain <jainbodhini05@gmail.com>
2025-04-10 00:17:38 +0530

    some changes

--- frontend/src/Pages/Home/About/About.jsx
+++ frontend/src/Pages/Home/About/About.jsx
@@ -20,15 +20,17 @@ function About({ backgroundC }) {
           
           {/* Right side - Text content */}
           <div className="text-content flex-grow">
+
+          <h1 className="bg-blue-700 w-fit py-1 px-3 rounded-sm my-2">
+              Our Story
+            </h1>
             <p>
               At ShieldGuard, we believe in the power of education to transform
               lives. Our platform is designed to be a gateway to knowledge,
               offering a diverse range of courses and learning experiences for
               students.
             </p>
-            <h1 className="bg-blue-700 w-fit py-1 px-3 rounded-sm my-2">
-              Our Story
-            </h1>
+           
             <p>
               ShieldGuard was born out of a passion for learning and a desire to
               make quality education accessible to everyone. We understand the
